2c9a981a52a05450c8c70fa3fedc5758a3781ebd
Antoine Montes <antoine.montes@epita.fr>
2021-02-03 23:36:37 +0100

    cpu: add circle percentage

---
+++ Perceval/wwwroot/js/globals.js
@@ -0,0 +1,8 @@
+﻿const cpuUsageHistoric = [];
+
+function addCpuUsage(newCpuUsage) {
+    if (cpuUsageHistoric.length >= 20) {
+        cpuUsageHistoric.shift();
+    }
+    cpuUsageHistoric.push(newCpuUsage);
+}
